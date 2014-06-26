b1ac8cecc8f9707acf764f68d7d71287be57e08f
Jon Carlson <jon@jomca.co>
2014-06-26 17:38:43 -0600

    Updates

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 $(document).on('ready', function() {
   
+  $('.value-text').on('click', function(){
+
+    var originalField = $(this);
+
+    var input = $('<textarea class="value-edit" />');
+    $(this).after(input);
+
+    input.height($(this).height());
+    $(this).hide();
+    input.val($(this).text());
+    input.focus();
+
+    input.on('blur', function(){
+      originalField.text(input.val());
+      input.remove();
+      originalField.show();
+    });
+
+  });
 });
